0823b776641cfb4cbbcc1020b7784f77681e11c2
weihang-wang <42722666+weihang-wang@users.noreply.github.com>
2018-10-08 11:52:39 -0400

    Update config.py

--- cse705/project/lenovo/config.py
+++ cse705/project/lenovo/config.py
@@ -5,6 +5,6 @@ def getConfig():
   if useOriginal == 1:
     return baseURL + "/lenovo/Y40_80.html"
   else:
-    return baseURL + "/lenovo/rand/Y40_80.html"
+    return baseURL + "Your_path_for_randomized_webpage"
 
     
