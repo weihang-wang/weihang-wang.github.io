852d7741d8ce1ad46d74752f59a40f1dbe518bef
weihang-wang <42722666+weihang-wang@users.noreply.github.com>
2022-01-10 21:53:31 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,33 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function openpage(evt, pagename) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabcontent.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(pagename).style.display = "block";
+	if( evt ) {
+		evt.currentTarget.className += " active";
+	}
+}
+
+
